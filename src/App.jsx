f7b76ed92e5f8baea3ecb3c9557e566c59e49b92
alkyleee <guevarrakyle@yahoo.com.ph>
2024-10-01 03:06:40 +0800

    added analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React from "react"
 import Navbar from "./components/Navbar"
 import AnimatedRoutes from "./components/AnimatedRoutes"
 import ScrollToTop from "./components/ScrollToTop"
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <>
+      <Analytics/>
       <Navbar />
       <ScrollToTop />
       <AnimatedRoutes />
