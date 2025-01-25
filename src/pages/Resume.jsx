2898b4c235907b842f20cd7509f0e4ca43c741e9
alkyleee <guevarrakyle@yahoo.com.ph>
2025-01-25 23:37:01 +0800

    Changed current to present

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -22,7 +22,7 @@ const Resume = () => {
           <p className='text-lg font-semibold text-tertiary'>Studied Bachelor of Science in Information Technology for four years at the University of Santo Tomas. Graduated a Cum Laude and achieved multiple Dean's Lister.</p>
         </ResumeCard>
         <p className="mt-20 text-2xl font-medium">Experience</p>
-        <ResumeCard duration='Sept 2024 - Current' positionName='Sales and Marketing Admin' companyDegree='Ampers Design & Printing' location='Bibra Lake, Western Australia'>
+        <ResumeCard duration='Sept 2024 - Present' positionName='Sales and Marketing Admin' companyDegree='Ampers Design & Printing' location='Bibra Lake, Western Australia'>
           <p className='text-lg font-semibold text-tertiary'>Sales and Marketing Admin for Ampers Design & Printing which includes managing their website, promotion of products, advertising, and more.</p>
         </ResumeCard>
         <ResumeCard duration='Nov 2023 - May 2024' positionName='Junior Associate - Digital Marketing' companyDegree='Neolytix' location='Quezon City, Philippines'>
