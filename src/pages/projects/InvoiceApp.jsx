9892b7ad0e2c27f703745db6fce67cc4cfd576af
alkyleee <guevarrakyle@yahoo.com.ph>
2025-05-26 23:55:26 +0800

    Typo fix

--- src/pages/projects/InvoiceApp.jsx
+++ src/pages/projects/InvoiceApp.jsx
@@ -4,7 +4,7 @@ import Img from '../../assets/images/InvoiceGenerator.jpg'
 const InvoiceGenerator = () => {
   return (
     <Project title="Invoice Generator" role="Full Stack Developer" year="2025" image={Img} link="https://invoice.alkyle.dev/">
-        Created an invoice generator application that allows users to create, manage, and send invoices. The application is built using NextJS and TailwindCSS. The database is manageed using IndexedDB for local storage.
+        Created an invoice generator application that allows users to create, manage, and save invoices. The application is built using NextJS and TailwindCSS. The database is manageed using IndexedDB for local storage.
     </Project>
   )
 }
